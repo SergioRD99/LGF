ba324744b81d28c075c8bbab17595b6fbcf88903
sergio ramos <sergio02.99ramos@gmail.com>
2024-02-27 18:53:27 -0600

    generando componentes
    integracion de jquery, bootstrap, y componentes

--- ALGF/src/app/app.module.ts
+++ ALGF/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductosComponent } from './home/productos/productos.component';
+import { ContactoComponent } from './home/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductosComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
